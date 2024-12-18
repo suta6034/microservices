7a5bc6f2c3ca129cfce854de78d43c5a04135e18
JW <suta6034@gmail.com>
2024-12-18 09:14:08 +0100

    JPA setup

---
+++ accounts/src/main/java/com/asten/accounts/entity/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.asten.accounts.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter @Setter @ToString
+public class BaseEntity {
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(updatable = false)
+    private String createdBy;
+
+    @Column(updatable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(insertable = false)
+    private String updatedBy;
+}
